035039e86a3c42c6109d89bbfbed98986993d217
ZeroOneZero29 <zhigadlov29@gmail.com>
2024-02-05 19:44:33 +0700

    push develop

--- public/js/coolback.js
+++ public/js/coolback.js
@@ -6,8 +6,6 @@ document.getElementById('coolback').addEventListener('submit', function(event) {
     var tel = document.getElementById('tel').value;
     var message = document.getElementById('message').value;
    
-    // Здесь будет код для отправки данных на сервер
-   
     alert('Сообщение отправлено!');
     this.reset();
   });
